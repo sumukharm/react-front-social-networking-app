4af79dfab19d5a1e27883c657ab74bc3d844952c
sumukharm <sumukha.mohan@digisignals.com>
2019-11-04 17:08:26 -0800

    welcome message

--- src/core/Home.js
+++ src/core/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
   <div>
     <div className="jumbotron">
       <h2>Home</h2>
-      <p className="lead">Welcome to React Frontend</p>
+      <p className="lead">Welcome to my Social Networking app!</p>
     </div>
     <div className="container">
       <Posts />
